7f21d6a5b97dd34dacf654d0e4acb94ac7091e5d
rbonilla <renato_bonilla@dai.com>
2022-08-24 13:55:54 -0600

    #1. Commit Inicial

---
+++ src/main/java/net/cuscatlan/cusbeerservice/CusBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.cusbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CusBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/cuscatlan/cusbeerservice/CusBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.cuscatlan.cusbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CusBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
